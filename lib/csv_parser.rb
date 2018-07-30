03487c141b8c7d35bad0abbee237e332ca10f2af
Sara Bocciardi <sjbocciardi@gmail.com>
2018-07-29 20:20:50 -0700

    Convert foo duration to number of seconds

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -12,6 +12,7 @@ class CSVParser
     parser = new filename
     parser.convert_timestamp
     parser.convert_zipcode
+    parser.convert_foo_duration_seconds
     parser.csv
   end
 
@@ -30,6 +31,16 @@ class CSVParser
   def convert_zipcode
     @csv = csv.each do |line|
       line[2] = line[2].rjust(ZIP_CODE_LENGTH, ZIP_CODE_PLACEHOLDER)
+    end
+  end
+
+  def convert_foo_duration_seconds
+    @csv = csv.each do |line|
+      duration = line[4].split(':')
+      hour_seconds = duration[0].to_i * 60 * 60
+      minute_seconds = duration[1].to_i * 60
+      second_seconds = duration[2].to_f
+      line[4] = hour_seconds + minute_seconds + second_seconds
     end
   end
 end
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -12,15 +12,15 @@ RSpec.describe CSVParser do
   describe '.parse' do
     let(:file_data) do
       [
-        ['1/1/11 12:00:01 AM', '', '1'],
-        ['12/31/16 11:59:59 PM', '', '94121'],
+        ['1/1/11 12:00:01 AM', '', '1', '', '1:23:32.123'],
+        ['12/31/16 11:59:59 PM', '', '94121', '', '0:0:0.1'],
       ]
     end
 
     it 'returns the csv format for the given file' do
       expect(CSVParser.parse(file_name)).to eq([
-        ['2011-01-01T03:00:01-05:00', '', '00001'],
-        ['2017-01-01T02:59:59-05:00', '', '94121']
+        ['2011-01-01T03:00:01-05:00', '', '00001', '', 5012.123],
+        ['2017-01-01T02:59:59-05:00', '', '94121', '', 0.1]
       ])
     end
   end
@@ -54,6 +54,23 @@ RSpec.describe CSVParser do
       expect(subject[0][2]).to eq('00001')
       expect(subject[1][2]).to eq('00581')
       expect(subject[2][2]).to eq('94121')
+    end
+  end
+
+  describe '#convert_foo_duration_to_seconds' do
+    let(:file_data) do
+      [
+        ['', '', '', '', '1:23:32.123'],
+        ['', '', '', '', '111:23:32.123'],
+        ['', '', '', '', '0:0:0.1'],
+      ]
+    end
+    subject { CSVParser.new(file_name).convert_foo_duration_seconds }
+
+    it 'converts duration to number of seconds' do
+      expect(subject[0][4]).to eq(5012.123)
+      expect(subject[1][4]).to eq(401012.123)
+      expect(subject[2][4]).to eq(0.1)
     end
   end
 end
