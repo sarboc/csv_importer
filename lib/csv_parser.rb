2ea916f28a4e2963c926730490ba734d39e08911
Sara Bocciardi <sjbocciardi@gmail.com>
2018-07-29 17:57:12 -0700

    Start writing parser with builder pattern
    - Make sure that it can accept a filename and return the data in that
    file

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,3 +1,14 @@
-class CSVParser
+require 'csv'
 
+class CSVParser
+  attr_reader :csv
+
+  def self.parse(filename)
+    parser = new filename
+    parser.csv
+  end
+
+  def initialize(filename)
+    @csv = CSV.read(filename)
+  end
 end
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -2,7 +2,16 @@ require 'rspec'
 require './lib/csv_parser'
 
 RSpec.describe CSVParser do
-  it "can init" do
-    expect(CSVParser.new.class).to eq(CSVParser)
+  describe '.parse' do
+    let(:file_name) { "test_file.csv" }
+    let(:file_data) { "My sample csv file data" }
+
+    before do
+      expect(CSV).to receive(:read).with(file_name).and_return(file_data)
+    end
+
+    it 'returns the csv format for the given file' do
+      expect(CSVParser.parse(file_name)).to eq(file_data)
+    end
   end
 end
